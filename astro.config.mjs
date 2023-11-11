6d357b17b423439f168577f52bb107062ea52b8c
Silas Gayner <Silas Gayner>
2023-11-11 11:32:46 -0800

    remove site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'static',
   adapter: vercelStatic(),
