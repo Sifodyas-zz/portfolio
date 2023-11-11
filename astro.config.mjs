4aaed2a3ba76e00fcad64d92464c3bf98b3e69cd
Silas Gayner <Silas Gayner>
2023-11-11 11:44:07 -0800

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
+import vercelStatic from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()],
